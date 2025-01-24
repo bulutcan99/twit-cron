95f84e9c4348d6f8dc9082d40182a4f934062b8e
bulutcan99 <gocerbulutcan@gmail.com>
2025-01-24 23:16:22 +0300

    refator

---
+++ target/debug/deps/twitter_cron-32bbb6d16404b5ed.d
@@ -0,0 +1,9 @@
+/Users/bulutgocer/PROJECTS/RUST/github/bulutcan99/twit-cron/target/debug/deps/libtwitter_cron-32bbb6d16404b5ed.rmeta: src/main.rs Cargo.toml
+
+/Users/bulutgocer/PROJECTS/RUST/github/bulutcan99/twit-cron/target/debug/deps/twitter_cron-32bbb6d16404b5ed.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
