3edac8a318ede6c5cec5bc361e6eda53a05d85fa
s84394880 <sevket.bulut.gocer@h-partners.com>
2025-02-05 16:37:42 +0300

    feat

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use anyhow::Result;
 use axum::{extract::State, routing::post, Json, Router};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, NaiveDateTime, Utc};
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
-use std::{env, net::SocketAddr, sync::Arc};
+use std::{
+    env,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+use tokio::sync::Notify;
+use tokio_cron_scheduler::{Job, JobScheduler};
 use twapi_v2::{
     api::{execute_twitter, post_2_tweets},
     oauth10a::OAuthAuthentication,
@@ -19,7 +25,7 @@ struct TweetRequest {
 #[derive(Debug, Deserialize)]
 struct TweetData {
     text: String,
-    scheduled_at: DateTime<Utc>, // ISO8601 formatında tarih beklenir
+    scheduled_at: NaiveDateTime, // ISO8601 formatında tarih beklenir
 }
 
 // Yanıt Formatı
@@ -62,7 +68,6 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-// Tweet Gönderme Endpoint'i
 async fn handle_tweet(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<TweetRequest>,
@@ -74,33 +79,75 @@ async fn handle_tweet(
         });
     }
 
-    let now = Utc::now();
-    for tweet in payload.tweets {
-        if tweet.scheduled_at < now {
+    let scheduler = match JobScheduler::new().await {
+        Ok(scheduler) => scheduler,
+        Err(_) => {
+            return Json(ApiResponse {
+                success: false,
+                message: "Scheduler not setted!".to_string(),
+            });
+        }
+    };
+
+    let notify = Arc::new(Notify::new());
+    let now = Utc::now() + Duration::hours(3);
+    let now_naive = now.naive_utc();
+    let job_size = payload.tweets.len();
+    let job_remaining = Arc::new(Mutex::new(job_size));
+
+    for (idx, tweet) in payload.tweets.iter().enumerate() {
+        if tweet.scheduled_at < now_naive {
             return Json(ApiResponse {
                 success: false,
                 message: "Tweet gönderme zamanı geçmiş olamaz!".to_string(),
             });
         }
 
+        let job_time = tweet.scheduled_at.format("%H:%M:%S").to_string();
+        let cron_timer = format!("{} * * * * *", job_time); // Cron formatını doğru şekilde oluşturun
+        let job_remaining = job_remaining.clone();
+        let notify = notify.clone();
+
+        // Tweet gönderme işlemi
         let body = post_2_tweets::Body {
-            text: Some(tweet.text),
+            text: Some(tweet.text.clone()),
             ..Default::default()
         };
 
         let builder = post_2_tweets::Api::new(body).build(&state.auth);
-        match execute_twitter::<serde_json::Value>(builder).await {
-            Ok((res, _)) => println!("Tweet başarıyla gönderildi: {:?}", res),
-            Err(e) => {
-                println!("Tweet gönderme hatası: {:?}", e);
-                return Json(ApiResponse {
-                    success: false,
-                    message: format!("Tweet gönderme hatası: {:?}", e),
-                });
-            }
-        };
+        let tweet_remaining = job_remaining.clone();
+        let notify = notify.clone();
+
+        // Cron job ekleyerek tweet gönderimini zamanla
+        if let Err(e) = scheduler.add(Job::new_async(cron_timer, move |uuid, mut l| {
+            Box::pin(async move {
+                // Tweet gönderme işlemi
+                match execute_twitter::<serde_json::Value>(builder).await {
+                    Ok((res, _)) => {
+                        println!("Tweet başarıyla gönderildi: {:?}", res);
+                    }
+                    Err(e) => {
+                        println!("Tweet gönderme hatası: {:?}", e);
+                    }
+                }
+
+                // İş tamamlandıktan sonra job_remaining sayacını güncelle
+                let mut remaining_jobs = tweet_remaining.lock().await;
+                *remaining_jobs -= 1;
+
+                // Tüm işler tamamlandığında notify tetikleyin
+                if *remaining_jobs == 0 {
+                    println!("Tüm işler tamamlandı.");
+                    notify.notify_one(); // Tüm işler bittiğinde sinyal gönder
+                }
+            })
+        })) {
+            // Eğer job eklerken hata alırsak, logla ve devam et
+            println!("Job eklenemedi: {:?}", e);
+        }
     }
 
+    // Kullanıcıya hemen planlama işleminin tamamlandığını bildir
     Json(ApiResponse {
         success: true,
         message: "Tweetler başarıyla planlandı!".to_string(),
